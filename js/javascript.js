6cd5f0d96fa596cad7feeaad6d8bf3c2e03fb8bf
Boris Pavel <boris.pavel@gmx.com>
2023-03-28 15:25:51 +0300

    Add event listeners

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,35 @@
 const container = document.querySelector('.etch-a-sketch');
+const btn = document.querySelector('#btn');
 
-
-
-for(let i = 0; i<16*16;++i){
-    const div = document.createElement('div');
-    div.style.cssText = 'background-color: white; border: 1px solid black; width: calc(100%/16); height: calc(100%/16); box-sizing: border-box;';
-    div.addEventListener('mouseover', () =>{
-        div.style.backgroundColor = 'black';
-    })
-    container.appendChild(div);
+function removeCurrentGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 }
+function createNewGrid(squares) {
+    removeCurrentGrid();
+    for (let i = 0; i < squares * squares; ++i) {
+        const div = document.createElement('div');
+        div.classList.add('etch-block');
+        div.style.width = `calc(100%/${squares})`;
+        div.style.height = `calc(100%/${squares})`;
+        div.addEventListener('mouseenter', () => {
+            div.classList.add('hover');
+        });
+        // div.addEventListener('mouseleave', () => {
+        //     div.classList.remove('hover');
+        // });
+        container.appendChild(div);
+    }
+}
+
+btn.addEventListener('click', () => {
+    let squares = prompt('How many squares do you want?', 'max 100');
+    if(squares < 101 && squares > 0){
+        createNewGrid(squares);
+    } else {
+        alert('Please enter a valid value!');
+    }
+    
+}
+);
